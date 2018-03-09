5f9b9748b2d29ccccb5349b7c584c68d95bc8109
Keli Hansen <keli.p.hansen@gmail.com>
2018-03-09 14:25:07 -0800

    add Handlebars compile call

--- public/scripts/articleView.js
+++ public/scripts/articleView.js
@@ -138,9 +138,9 @@
     };
 
     articleView.initAdminPage = () => {
-        // TODO: Call the Handlebars .compile() method, which will return a function for you to use where needed.
+        // TODOne: Call the Handlebars .compile() method, which will return a function for you to use where needed.
         // Make sure you assign the result of your Handlebars.compile call to a variable called "template", since we are then calling "template" below.
-
+        const template = Handlebars.compile($('#article-template').text());
         // REVIEW: We use .forEach() here because we are relying on the side-effects of the callback function: appending to the DOM.
         // The callback is not required to return anything.
         app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
