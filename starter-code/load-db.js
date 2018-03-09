7b32053ff65020eb004794f94f56393c082f2c9a
martypdx <marty@martypdx.com>
2018-03-08 23:37:48 -0800

    clean up server code

---
+++ starter-code/load-db.js
@@ -0,0 +1,93 @@
+'use strict';
+const dotenv = require('dotenv');
+dotenv.config();
+
+const pg = require('pg');
+const Client = pg.Client;
+const fs = require('fs');
+const util = require('util');
+const promisify = util.promisify;
+const readFile = promisify(fs.readFile);
+
+const client = new Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => {
+    console.error(err);
+});
+
+loadDB()
+    .then(() => console.log('loadDB successful'))
+    .catch(err => console.error(err))
+    .then(() => client.end());
+
+function loadDB() {
+    return Promise.all([
+        client.query(`
+            CREATE TABLE IF NOT EXISTS
+            authors (
+                author_id SERIAL PRIMARY KEY,
+                author VARCHAR(255) UNIQUE NOT NULL,
+                author_url VARCHAR (255)
+            );
+        `),
+        client.query(`
+            CREATE TABLE IF NOT EXISTS
+            articles (
+                article_id SERIAL PRIMARY KEY,
+                author_id INTEGER NOT NULL REFERENCES authors(author_id),
+                title VARCHAR(255) NOT NULL,
+                category VARCHAR(20),
+                published_on DATE,
+                body TEXT NOT NULL
+            );
+        `)
+    ])
+        .then(() => loadAuthors())
+        .then(() => loadArticles());
+}
+
+function loadAuthors() {
+    return loadIfNoRows(
+        'authors',
+        `INSERT INTO authors(author, author_url) 
+        VALUES($1, $2) 
+        ON CONFLICT DO NOTHING;`,
+        article => [article.author, article.authorUrl]
+    );
+}
+
+function loadArticles() {
+    return loadIfNoRows(
+        'articles',
+        `INSERT INTO
+        articles(author_id, title, category, published_on, body)
+        SELECT author_id, $1, $2, $3, $4
+        FROM authors
+        WHERE author=$5;`,
+        article => [article.title, article.category, article.publishedOn, article.body, article.author]
+    );
+}
+
+function readHackerIpsum() {
+    return readFile('./public/data/hackerIpsum.json', 'utf8')
+        .then(json => JSON.parse(json));
+}
+
+function loadIfNoRows(table, insertSql, values) {
+    return client.query(`SELECT COUNT(*) FROM ${table};`)
+        .then(result => {
+            if(parseInt(result.rows[0].count)) return;
+
+            return readHackerIpsum()
+                .then(articles => {
+                    return Promise.all(
+                        articles.map(article => {
+                            return client.query(
+                                insertSql,
+                                values(article)
+                            );
+                        })
+                    );
+                });
+        });
+}
--- starter-code/server.js
+++ starter-code/server.js
@@ -1,105 +1,124 @@
 'use strict';
-// REVIEW: Check out all of our new arrow function syntax!
 
-const pg = require('pg');
-const fs = require('fs');
-const express = require('express');
-const bodyParser = require('body-parser');
+/* Environment variables */
+require('dotenv').config(); // load from .env
 const PORT = process.env.PORT || 3000;
+// TODO: Don't forget to set your own conString in .env
+const DATABASE_URL = process.env.DATABASE_URL;
+
+/* Required Dependencies */
+const pg = require('pg');
+const express = require('express');
 const app = express();
-// TODO: Don't forget to set your own conString
-const conString = '';
-const client = new pg.Client(conString);
+
+/* DB Connection */
+const client = new pg.Client(DATABASE_URL);
 client.connect();
 client.on('error', err => {
     console.error(err);
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+/* Express Middleware */
+// body parsers:
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+// serve static assets:
 app.use(express.static('./public'));
 
-app.get('/new', (request, response) => response.sendFile('new.html', {root: './public'}));
-app.get('/admin', (request, response) => response.sendFile('admin.html', {root: './public'}));
+
+/** Data API Routes **/
+
+// (R)ead list of articles
 app.get('/articles', (request, response) => {
     client.query(`
-    SELECT * FROM articles
-    INNER JOIN authors
-      ON articles.author_id=authors.author_id;`
-    )
+        SELECT * FROM articles
+        INNER JOIN authors
+        ON articles.author_id=authors.author_id;
+    `)
         .then(result => response.send(result.rows))
         .catch(console.error);
 });
 
+// (C)reate new article, and possibly author
 app.post('/articles', (request, response) => {
-    client.query(
-        'INSERT INTO authors(author, "authorUrl") VALUES($1, $2) ON CONFLICT DO NOTHING',
-        [request.body.author, request.body.authorUrl],
-        function(err) {
-            if (err) console.error(err);
-            queryTwo();
-        }
-    );
+    const body = request.body;
 
-    function queryTwo() {
-        client.query(
-            `SELECT author_id FROM authors WHERE author=$1`,
-            [request.body.author],
-            function(err, result) {
-                if (err) console.error(err);
-                queryThree(result.rows[0].author_id);
-            }
-        );
-    }
-
-    function queryThree(author_id) {
-        client.query(
-            `INSERT INTO
-      articles(author_id, title, category, "publishedOn", body)
-      VALUES ($1, $2, $3, $4, $5);`,
+    resolveAuthor(body.author, body.authorUrl)
+        .then(author_id => {
+            return client.query(`
+                INSERT INTO
+                articles(author_id, title, category, published_on, body)
+                VALUES ($1, $2, $3, $4, $5);
+            `,
             [
                 author_id,
-                request.body.title,
-                request.body.category,
-                request.body.publishedOn,
-                request.body.body
-            ],
-            function(err) {
-                if (err) console.error(err);
-                response.send('insert complete');
-            }
-        );
-    }
+                body.title,
+                body.category,
+                body.publishedOn,
+                body.body
+            ]);
+        })
+        .then(() => {
+            response.send('insert complete');
+        })
+        .catch(console.error);
 });
 
-app.put('/articles/:id', (request, response) => {
-    client.query(`
-    UPDATE authors
-    SET author=$1, "authorUrl"=$2
-    WHERE author_id=$3
-    `,
-        [request.body.author, request.body.authorUrl, request.body.author_id]
+// helper function to read or create author based on name
+function resolveAuthor(author, authorUrl) {
+    return client.query(
+        `SELECT author_id FROM authors WHERE author=$1`,
+        [author]
     )
-        .then(() => {
-            client.query(`
-      UPDATE articles
-      SET author_id=$1, title=$2, category=$3, "publishedOn"=$4, body=$5
-      WHERE article_id=$6
-      `,
-                [
-                    request.body.author_id,
-                    request.body.title,
-                    request.body.category,
-                    request.body.publishedOn,
-                    request.body.body,
-                    request.params.id
-                ]
+        .then(result => {
+            if(result.rows.length > 0) return result;
+
+            return client.query(`
+                INSERT INTO authors(author, author_url) 
+                VALUES($1, $2)
+                RETURNING author_id
+            `,
+            [author, authorUrl]
             );
         })
+        .then(result => result.rows[0].author_id);
+}
+
+// (U)pdate article (and author)
+app.put('/articles/:id', (request, response) => {
+    const body = request.body;
+    const params = request.params;
+
+    Promise.all([
+        client.query(`
+            UPDATE authors
+            SET author=$1, author_url=$2
+            WHERE author_id=$3
+        `,
+        [
+            body.author,
+            body.authorUrl,
+            body.author_id
+        ]),
+        client.query(`
+                UPDATE articles
+                SET author_id=$1, title=$2, category=$3, published_on=$4, body=$5
+                WHERE article_id=$6
+        `,
+        [
+            body.author_id,
+            body.title,
+            body.category,
+            body.publishedOn,
+            body.body,
+            params.id
+        ])
+    ])
         .then(() => response.send('Update complete'))
         .catch(console.error);
 });
 
+// (D)estroy an article
 app.delete('/articles/:id', (request, response) => {
     client.query(
         `DELETE FROM articles WHERE article_id=$1;`,
@@ -109,76 +128,13 @@ app.delete('/articles/:id', (request, response) => {
         .catch(console.error);
 });
 
+// (D)estroy ALL articles
 app.delete('/articles', (request, response) => {
-    client.query('DELETE FROM articles')
-        .then(() => response.send('Delete complete'))
+    client.query(`DELETE FROM articles`)
+        .then(() => response.send('Full Delete Complete'))
         .catch(console.error);
 });
 
-loadDB();
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
-
-
-//////// ** DATABASE LOADERS ** ////////
-////////////////////////////////////////
-function loadAuthors() {
-    fs.readFile('./public/data/hackerIpsum.json', (err, fd) => {
-        JSON.parse(fd.toString()).forEach(ele => {
-            client.query(
-                'INSERT INTO authors(author, "authorUrl") VALUES($1, $2) ON CONFLICT DO NOTHING',
-                [ele.author, ele.authorUrl]
-            )
-                .catch(console.error);
-        });
-    });
-}
-
-function loadArticles() {
-    client.query('SELECT COUNT(*) FROM articles')
-        .then(result => {
-            if(!parseInt(result.rows[0].count)) {
-                fs.readFile('./public/data/hackerIpsum.json', (err, fd) => {
-                    JSON.parse(fd.toString()).forEach(ele => {
-                        client.query(`
-            INSERT INTO
-            articles(author_id, title, category, "publishedOn", body)
-            SELECT author_id, $1, $2, $3, $4
-            FROM authors
-            WHERE author=$5;
-          `,
-                            [ele.title, ele.category, ele.publishedOn, ele.body, ele.author]
-                        )
-                            .catch(console.error);
-                    });
-                });
-            }
-        });
-}
-
-function loadDB() {
-    client.query(`
-    CREATE TABLE IF NOT EXISTS
-    authors (
-      author_id SERIAL PRIMARY KEY,
-      author VARCHAR(255) UNIQUE NOT NULL,
-      "authorUrl" VARCHAR (255)
-    );`
-    )
-        .then(loadAuthors)
-        .catch(console.error);
-
-    client.query(`
-    CREATE TABLE IF NOT EXISTS
-    articles (
-      article_id SERIAL PRIMARY KEY,
-      author_id INTEGER NOT NULL REFERENCES authors(author_id),
-      title VARCHAR(255) NOT NULL,
-      category VARCHAR(20),
-      "publishedOn" DATE,
-      body TEXT NOT NULL
-    );`
-    )
-        .then(loadArticles)
-        .catch(console.error);
-}
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}!`);
+});
