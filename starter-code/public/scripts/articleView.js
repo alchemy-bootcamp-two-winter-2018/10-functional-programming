01b23c67f584b8dc91afae0611e2848a5c78f6ea
Victor Bofill <terranvas@gmail.com>
2018-03-09 16:41:36 -0800

    Got admin page working

--- starter-code/public/scripts/articleView.js
+++ starter-code/public/scripts/articleView.js
@@ -140,8 +140,8 @@
     // TODONE: Call the Handlebars .compile() method, which will return a function for you to use where needed.
     // Make sure you assign the result of your Handlebars.compile call to a variable called "template", since we are then calling "template" below.
 
-        const template = Handlebars.compile();
-
+        const template = Handlebars.compile($('#author-template').text());
+        
         // REVIEW: We use .forEach() here because we are relying on the side-effects of the callback function: appending to the DOM.
         // The callback is not required to return anything.
         app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
