f3c0c3bb406861719f6edfb7a644fc32a386002f
Steele Walston <steele.walston@gmail.com>
2018-03-17 15:03:28 -0700

    got handlebars compile

--- public/scripts/articleView.js
+++ public/scripts/articleView.js
@@ -140,13 +140,13 @@
   };
 
   articleView.initAdminPage = () => {
-    // TODO: Call the Handlebars .compile() method, which will return a function for you to use where needed.
+    // TODOne: Call the Handlebars .compile() method, which will return a function for you to use where needed.
     // Make sure you assign the result of your Handlebars.compile call to a variable called "template", since we are then calling "template" below.
-    const template =
+    const template = Handlebars.compile($('#stats-template').html());
 
-        // REVIEW: We use .forEach() here because we are relying on the side-effects of the callback function: appending to the DOM.
-        // The callback is not required to return anything.
-        Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
+    // REVIEW: We use .forEach() here because we are relying on the side-effects of the callback function: appending to the DOM.
+    // The callback is not required to return anything.
+    Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
 
     // REVIEW: Simply write the correct values to the page:
     $('#blog-stats .articles').text(Article.all.length);
